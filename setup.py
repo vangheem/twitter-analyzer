b493b0b1b2d10b32685b3f05b8cfbc4bf4e97d32
vangheem <vangheem@gmail.com>
2018-10-27 23:15:21 -0400

    tweaks

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
-        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules'
